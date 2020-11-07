bf18927dce7f56bc3186a1f83eb2d6e124f2018e
Srivatsav Bendi <43092343+Srivatsav55@users.noreply.github.com>
2020-11-07 15:38:54 -0500

    Update firebase1.js

--- firebase1.js
+++ firebase1.js
@@ -9,30 +9,6 @@ if(profileusernamelength>1){
   description.innerHTML = "";
 }
 
-$(document).ready(function() { 
-  var id = '#dialog';
-  var maskHeight = $(document).height();
-  var maskWidth = $(window).width();
-  $('#mask').css({'width':maskWidth,'height':maskHeight}); 
-  $('#mask').fadeIn(500); 
-  $('#mask').fadeTo("slow",0.9); 
-        var winH = $(window).height();
-  var winW = $(window).width();
-        $(id).css('top',  winH/2-$(id).height()/2);
-  $(id).css('left', winW/2-$(id).width()/2);
-     $(id).fadeIn(2000);  
-     $('.window .close').click(function (e) {
-  e.preventDefault();
-  $('#mask').hide();
-  $('.window').hide();
-     });  
-     $('#mask').click(function () {
-  $(this).hide();
-  $('.window').hide();
- });  
- 
-});
-
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyB94h7P-9hQe5nsYKRR0dJOYO2azUnBpHs",
